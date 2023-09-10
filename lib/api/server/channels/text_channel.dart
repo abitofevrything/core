b1c9d4b4b4cfff1295fe46445223f76f023f194b
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-09-10 21:28:45 +0200

    feat: introduce guild text channel

--- lib/api/server/channels/text_channel.dart
+++ lib/api/server/channels/text_channel.dart
@@ -1,1 +1,20 @@
-class TextChannel {} // todo
+import '../cache/guild_message_cache.dart';
+import 'guild_channel.dart';
+
+class TextChannel extends GuildChannel {
+  GuildMessageCache messages = GuildMessageCacheEntity();
+  bool? nsfw;
+  int? rateLimitPerUser;
+  String? topic;
+
+  TextChannel(super.id, super.label, {
+    required super.guild,
+    required super.categoryChannel,
+    required super.permissions,
+    required super.flags,
+    required super.position,
+    required this.nsfw,
+    required this.rateLimitPerUser,
+    required this.topic
+  });
+}
