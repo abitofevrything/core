7bd6122ff00b7085e7d6b358907e310dfc12d622
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-09-10 21:29:07 +0200

    feat: introduce toJson, getters and cache

--- lib/api/server/guild.dart
+++ lib/api/server/guild.dart
@@ -1,15 +1,17 @@
-import 'package:mineral/api/server/channels/text_channel.dart';
-import 'package:mineral/api/channels/guild/voice_channel.dart';
-import 'package:mineral/api/guild/enum/locale.dart';
 import 'package:mineral/api/managers/guild_managers/channels_manager.dart';
 import 'package:mineral/api/managers/guild_managers/emojis_manager.dart';
 import 'package:mineral/api/managers/guild_managers/roles_manager.dart';
 import 'package:mineral/api/private/user.dart';
+import 'package:mineral/api/server/channels/text_channel.dart';
+import 'package:mineral/api/server/resources/guild_enum.dart';
+import 'package:mineral/api/server/resources/guild_features.dart';
+import 'package:mineral/api/server/resources/locale.dart';
+import 'package:mineral/api/shared/snowflake.dart';
+import 'package:mineral/internal/mixins/collection.dart';
 
 import '../managers/guild_managers/member_manager.dart';
 import '../managers/image_formatter.dart';
-import '../guild/enum/guild_enum.dart';
-import '../guild/enum/guild_features.dart';
+import 'channels/voice_channel.dart';
 
 final class Guild {
   final String id;
@@ -17,25 +19,26 @@ final class Guild {
   String? description;
   User owner;
 
-  final MemberManager members;
-  final RolesManager roles;
-  final ChannelsManager channels;
-  final EmojisManager emojis;
+  final MemberManager members = MemberManager();
+  final RolesManager roles = RolesManager();
+  final ChannelsManager channels = ChannelsManager();
+  final EmojisManager emojis = EmojisManager();
 
   ImageFormatter? icon;
   ImageFormatter? banner;
+  ImageFormatter? splash;
 
-  VoiceChannel? afkChannel;
+  Snowflake? afkChannelId;
   int? afkTimeout;
 
-  TextChannel? rulesChannel;
-  TextChannel? systemChannel;
-  TextChannel? publicUpdatesChannel;
-  TextChannel? systemChannelFlags;
-  TextChannel? safetyAlertsChannel;
+  Snowflake? rulesChannelId;
+  Snowflake? systemChannelId;
+  Snowflake? publicUpdatesChannelId;
+  Snowflake? systemChannelFlagsId;
+  Snowflake? safetyAlertsChannelId;
 
   bool? widgetEnabled;
-  TextChannel? widgetChannel;
+  Snowflake? widgetChannelId;
 
   VerificationLevel verificationLevel;
   NotificationLevel defaultNotificationLevel;
@@ -57,23 +60,23 @@ final class Guild {
   int? approximatePresenceCount;
   bool premiumProgressBarEnabled;
 
-  Guild({
+  Guild._({
     required this.id,
     required this.label,
     required this.description,
     required this.owner,
-    required this.members,
-    required this.roles,
-    required this.channels,
     required this.icon,
     required this.banner,
-    required this.afkChannel,
+    required this.splash,
+    required this.afkChannelId,
     required this.afkTimeout,
-    required this.rulesChannel,
-    required this.systemChannel,
-    required this.publicUpdatesChannel,
+    required this.rulesChannelId,
+    required this.systemChannelId,
+    required this.publicUpdatesChannelId,
+    required this.systemChannelFlagsId,
+    required this.safetyAlertsChannelId,
     required this.widgetEnabled,
-    required this.widgetChannel,
+    required this.widgetChannelId,
     required this.verificationLevel,
     required this.defaultNotificationLevel,
     required this.explicitContentFilter,
@@ -92,8 +95,38 @@ final class Guild {
     required this.approximateMemberCount,
     required this.approximatePresenceCount,
     required this.premiumProgressBarEnabled,
-    required this.emojis,
-    required this.systemChannelFlags,
-    required this.safetyAlertsChannel,
   });
+
+  TextChannel? get systemChannel => channels.cache.get(systemChannelId.toString());
+  TextChannel? get publicUpdatesChannel => channels.cache.get(publicUpdatesChannelId.toString());
+  TextChannel? get safetyAlertsChannel => channels.cache.get(safetyAlertsChannelId.toString());
+  TextChannel? get widgetChannel => channels.cache.get(widgetChannelId.toString());
+  VoiceChannel? get afkChannel => channels.cache.get(afkChannelId.toString());
+  VoiceChannel? get rulesChannel => channels.cache.get(rulesChannelId.toString());
+
+  /// [Guild] to Json method is private
+  dynamic _toJson() {
+    return {
+      "name": label,
+      "description": description,
+      "icon": icon,
+      "banner": banner,
+      "verification_level": verificationLevel.value,
+      "default_message_notifications": defaultNotificationLevel.value,
+      "explicit_content_filter": explicitContentFilter.value,
+      "afk_channel_id": afkChannelId,
+      "afk_timeout": afkTimeout,
+      "owner_id": owner.id,
+      "splash": splash,
+      "discovery_splash": splash,
+      "system_channel_id": systemChannelId,
+      "system_channel_flags": systemChannelFlagsId,
+      "rules_channel_id": rulesChannelId,
+      "public_updates_channel_id": publicUpdatesChannelId,
+      "preferred_locale": preferredLocale.locale,
+      "features": features.map((e) => e.name),
+      "premium_progress_bar_enabled": premiumProgressBarEnabled,
+      "safety_alerts_channel_id": safetyAlertsChannelId,
+    };
+  }
 }
