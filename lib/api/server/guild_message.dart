6d49f3d3d3f7836ade8f10694f3ba4ab2b154116
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-09-11 14:43:21 +0200

    feat: init guild message

--- lib/api/server/guild_message.dart
+++ lib/api/server/guild_message.dart
@@ -1,4 +1,31 @@
+import 'package:mineral/api/server/channels/text_channel.dart';
+import 'package:mineral/api/server/guild.dart';
+import 'package:mineral/api/server/guild_member.dart';
 import 'package:mineral/api/shared/message.dart';
 
 final class GuildMessage extends Message {
+  final TextChannel channel;
+  final Guild guild;
+  final GuildMember member;
+  List<Null> roles; // todo implement roles
+
+  GuildMessage._({
+      required super.id,
+      required super.content,
+      required super.user,
+      required super.timestamp,
+      required super.editedTimestamp,
+      required super.tts,
+      required super.mentionEveryone,
+      required super.mentions,
+      required super.pinned,
+      required super.type,
+      required super.reactions,
+      required super.attachments,
+      required super.embeds,
+      required this.channel,
+      required this.guild,
+      required this.member,
+      required this.roles
+  });
 }
