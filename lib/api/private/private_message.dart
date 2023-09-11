c2b752cfdb02610b68a4885676aacfa869602c06
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-09-11 14:46:55 +0200

    fix: introduice super params

--- lib/api/private/private_message.dart
+++ lib/api/private/private_message.dart
@@ -1,3 +1,5 @@
 import 'package:mineral/api/shared/message.dart';
 
-final class PrivateMessage extends Message {}
+final class PrivateMessage extends Message {
+  PrivateMessage({required super.id, required super.content, required super.user, required super.timestamp, required super.editedTimestamp, required super.tts, required super.mentionEveryone, required super.mentions, required super.pinned, required super.type, required super.reactions, required super.attachments, required super.embeds});
+}
