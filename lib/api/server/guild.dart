4619b75e9cb0695f4044c46a0fd73eb3983de201
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-09-13 20:43:40 +0200

    feat: remove user owner and from method work

--- lib/api/server/guild.dart
+++ lib/api/server/guild.dart
@@ -1,23 +1,20 @@
 import 'package:mineral/api/managers/guild_managers/channels_manager.dart';
 import 'package:mineral/api/managers/guild_managers/emojis_manager.dart';
+import 'package:mineral/api/managers/guild_managers/member_manager.dart';
 import 'package:mineral/api/managers/guild_managers/roles_manager.dart';
-import 'package:mineral/api/private/user.dart';
+import 'package:mineral/api/managers/image_formatter.dart';
 import 'package:mineral/api/server/channels/text_channel.dart';
-import 'package:mineral/api/server/resources/guild_enum.dart';
-import 'package:mineral/api/server/resources/guild_features.dart';
-import 'package:mineral/api/server/resources/locale.dart';
+import 'package:mineral/api/server/channels/voice_channel.dart';
+import 'package:mineral/api/server/resources/enums.dart';
+import 'package:mineral/api/server/resources/vanity.dart';
 import 'package:mineral/api/shared/snowflake.dart';
 import 'package:mineral/internal/mixins/collection.dart';
-
-import '../managers/guild_managers/member_manager.dart';
-import '../managers/image_formatter.dart';
-import 'channels/voice_channel.dart';
 
 final class Guild {
   final String id;
   String label;
   String? description;
-  User owner;
+  Snowflake ownerId;
 
   final MemberManager members = MemberManager();
   final RolesManager roles = RolesManager();
@@ -32,10 +29,10 @@ final class Guild {
   int? afkTimeout;
 
   Snowflake? rulesChannelId;
-  Snowflake? systemChannelId;
   Snowflake? publicUpdatesChannelId;
-  Snowflake? systemChannelFlagsId;
   Snowflake? safetyAlertsChannelId;
+  Snowflake? systemChannelId;
+  int? systemChannelFlags;
 
   bool? widgetEnabled;
   Snowflake? widgetChannelId;
@@ -48,11 +45,11 @@ final class Guild {
   PremiumTier premiumTier;
   Locale preferredLocale;
   List<GuildFeature> features;
+  Vanity? vanity;
 
   String? applicationId;
   int? maxPresences;
   int? maxMembers;
-  String vanityUrlCode;
   int? premiumSubscriptionCount;
   int? maxVideoChannelUsers;
   int? maxStageVideoChannelUsers;
@@ -64,7 +61,7 @@ final class Guild {
     required this.id,
     required this.label,
     required this.description,
-    required this.owner,
+    required this.ownerId,
     required this.icon,
     required this.banner,
     required this.splash,
@@ -73,7 +70,7 @@ final class Guild {
     required this.rulesChannelId,
     required this.systemChannelId,
     required this.publicUpdatesChannelId,
-    required this.systemChannelFlagsId,
+    required this.systemChannelFlags,
     required this.safetyAlertsChannelId,
     required this.widgetEnabled,
     required this.widgetChannelId,
@@ -88,7 +85,6 @@ final class Guild {
     required this.applicationId,
     required this.maxPresences,
     required this.maxMembers,
-    required this.vanityUrlCode,
     required this.premiumSubscriptionCount,
     required this.maxVideoChannelUsers,
     required this.maxStageVideoChannelUsers,
@@ -116,11 +112,11 @@ final class Guild {
       "explicit_content_filter": explicitContentFilter.value,
       "afk_channel_id": afkChannelId,
       "afk_timeout": afkTimeout,
-      "owner_id": owner.id,
+      "owner_id": ownerId,
       "splash": splash,
       "discovery_splash": splash,
       "system_channel_id": systemChannelId,
-      "system_channel_flags": systemChannelFlagsId,
+      "system_channel_flags": systemChannelFlags,
       "rules_channel_id": rulesChannelId,
       "public_updates_channel_id": publicUpdatesChannelId,
       "preferred_locale": preferredLocale.locale,
@@ -128,5 +124,53 @@ final class Guild {
       "premium_progress_bar_enabled": premiumProgressBarEnabled,
       "safety_alerts_channel_id": safetyAlertsChannelId,
     };
+  }
+
+  // not tested
+  factory Guild.from(final payload) {
+    List<GuildFeature> features = [];
+
+    if (payload["features"] != null) {
+      for (final feature in payload["features"]) {
+        GuildFeature? guildFeature = GuildFeature.from(feature);
+        if (guildFeature != null) features.add(guildFeature);
+      }
+    }
+
+    return Guild._(
+        id: payload["id"],
+        label: payload["name"],
+        description: payload["description"],
+        ownerId: payload["owner_id"],
+        icon: ImageFormatter(payload["icon"]),
+        banner: ImageFormatter(payload["banner"]),
+        splash: ImageFormatter(payload["splash"]),
+        afkChannelId: payload["afk_channel_id"],
+        afkTimeout: payload["afk_timeout"],
+        rulesChannelId: payload["rules_channel_id"],
+        systemChannelId: payload["system_channel_id"],
+        publicUpdatesChannelId: payload["public_updates_channel_id"],
+        systemChannelFlags: payload["system_channel_flags"],
+        safetyAlertsChannelId: payload["safety_alerts_channel_id"],
+        widgetEnabled: payload["widget_enabled"],
+        widgetChannelId: payload["widget_channel_id"],
+        verificationLevel: VerificationLevel.from(payload["verification_level"]),
+        defaultNotificationLevel: NotificationLevel.from(payload["default_message_notifications"]),
+        explicitContentFilter: ContentFilterLevel.from(payload["explicit_content_filter"]),
+        nsfwLevel: NSFWLevel.from(payload["nsfw_level"]),
+        mfaLevel: MFALevel.from(payload["mfa_level"]),
+        premiumTier: PremiumTier.from(payload["premium_tier"]),
+        preferredLocale: Locale.from(payload["preferred_locale"]),
+        features: features,
+        applicationId: payload["application_id"],
+        maxPresences: payload["max_presences"],
+        maxMembers: payload["max_members"],
+        premiumSubscriptionCount: payload["premium_subscription_count"],
+        maxVideoChannelUsers: payload["max_video_channel_users"],
+        maxStageVideoChannelUsers: payload["max_stage_video_channel_users"],
+        approximateMemberCount: payload["approximate_member_count"],
+        approximatePresenceCount: payload["approximate_presence_count"],
+        premiumProgressBarEnabled: payload["premium_progress_bar_enabled"]
+    );
   }
 }
