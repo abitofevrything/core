ab2a7b3298f4cade3e1d0a292227777d78b9cc3d
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-09-13 20:41:21 +0200

    feat: user's mentions in messages

--- lib/api/private/private_message.dart
+++ lib/api/private/private_message.dart
@@ -1,5 +1,22 @@
+import 'package:mineral/api/private/user.dart';
 import 'package:mineral/api/shared/message.dart';
 
 final class PrivateMessage extends Message {
-  PrivateMessage({required super.id, required super.content, required super.user, required super.timestamp, required super.editedTimestamp, required super.tts, required super.mentionEveryone, required super.mentions, required super.pinned, required super.type, required super.reactions, required super.attachments, required super.embeds});
+  List<User> mentions;
+
+  PrivateMessage({
+      required super.id,
+      required super.content,
+      required super.user,
+      required super.timestamp,
+      required super.editedTimestamp,
+      required super.tts,
+      required super.mentionEveryone,
+      required super.pinned,
+      required super.type,
+      required super.reactions,
+      required super.attachments,
+      required super.embeds,
+      required this.mentions,
+  });
 }
